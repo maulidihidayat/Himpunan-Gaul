9107b3bcacc7674bd18597b0f96abf37d4d51dfe
maulidihidayat <maulidi9999@gmail.com>
2025-07-30 02:20:12 +0700

    fixed last

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,6 @@ export default defineConfig({
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
-      build: {
-       manifest: true,
-       outDir: 'public/build',
-       emptyOutDir: true,
-   },
     }),
     
   ],
